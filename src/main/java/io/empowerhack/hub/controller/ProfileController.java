3ba89288f019c2930f3ce81e463ce7a7509783b8
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-18 07:56:44 +0000

    Removed unused includes

--- src/main/java/io/empowerhack/hub/controller/ProfileController.java
+++ src/main/java/io/empowerhack/hub/controller/ProfileController.java
@@ -8,13 +8,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.security.Principal;
 
 @Controller
 @EnableOAuth2Sso
