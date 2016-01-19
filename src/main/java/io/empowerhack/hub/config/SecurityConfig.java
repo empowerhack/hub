519fade1ca6e74070a0de9eb94d56b7df5334967
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-19 08:43:59 +0000

    Auth Listener to register user in App, if they do not exist

--- src/main/java/io/empowerhack/hub/config/SecurityConfig.java
+++ src/main/java/io/empowerhack/hub/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.empowerhack.hub.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,6 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     OAuth2ClientContext oauth2ClientContext;
+
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -57,11 +61,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private Filter ssoFilter(ClientResourcesConfig client, String path) {
         OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter(
                 path);
-        OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(client.getClient(),
-                oauth2ClientContext);
+        OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(client.getClient(), this.oauth2ClientContext);
+        githubFilter.setApplicationEventPublisher(this.applicationEventPublisher);
         githubFilter.setRestTemplate(githubTemplate);
-        githubFilter.setTokenServices(new UserInfoTokenServices(
-                client.getResource().getUserInfoUri(), client.getClient().getClientId()));
+        githubFilter.setTokenServices(
+                new UserInfoTokenServices(
+                        client.getResource().getUserInfoUri(), client.getClient().getClientId()
+                )
+        );
         return githubFilter;
     }
 }
